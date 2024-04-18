a7695ed6afd2e8de8ccfa9f40375e4b7cb2d04de
Dmitry Bogatov <bogatovdma@gmail.com>
2024-04-18 18:45:56 +0300

    Удалил аудио

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <>
     <div className = 'bg-[#292524] w-screen h-screen text-[#bdb595] font-sans'>
       <header className='bg-[#1c1917] container mx-auto flex'>
-      <audio  src="../music/bg_music_rusy_protiv_yacsherov.mp3"/>
       
         <div className='flex justify-between pt-50px items-center'>
           <img src='../images/logo.png' className='w-7 h-6'/>
